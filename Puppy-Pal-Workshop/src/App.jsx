66e0d52e44fce3df01bef96d9a7335beca9e4a29
nanaaoki <aoki.nana@gmail.com>
2023-12-17 10:30:47 -0800

    completed workshop

--- Puppy-Pal-Workshop/src/App.jsx
+++ Puppy-Pal-Workshop/src/App.jsx
@@ -1,15 +1,46 @@
+import { puppyList } from "./data.js";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  console.log("puppies: ", puppies);
 
   return (
-    <>
-      <div></div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <div className="grid1">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+              className="puppyList"
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
+      <div className="grid2">
+        {featuredPup && (
+          <div className="featuredPup">
+            <h2 className="featuredName">{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+    </div>
   );
 }
 
